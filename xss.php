de485f31943ffc4c10a2ba54a59a695f894630ae
infocyberhacker <61610012+infocyberhacker@users.noreply.github.com>
2020-03-17 00:50:05 +0700

    Update xss.php

--- xss.php
+++ xss.php
@@ -2,7 +2,7 @@
 	if(@$_POST['z']{
 	echo 'hasil pencaharian: '.$_POST['z'];
 	}else{
-	echo'<form action="" method="post">
+	echo '<form action="" method="post">
 	<input type="text" name="z">
 	<input type="sumbit" value="cari">
 	';
