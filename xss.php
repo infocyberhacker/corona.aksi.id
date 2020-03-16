2545f25544997c50c23ac228a72c3d3b429c59c2
infocyberhacker <61610012+infocyberhacker@users.noreply.github.com>
2020-03-17 00:47:02 +0700

    Add files via upload

---
+++ xss.php
@@ -0,0 +1,10 @@
+<?php
+	if(@$_POST['z']{
+	echo 'hasil pencaharian: '.$_POST['z'];
+	}else{
+	echo'<form action="" method="post">
+	<input type="text" name="z">
+	<input type="sumbit" value="cari">
+	';
+	}
+ ?>
